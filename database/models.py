6663d66b90e8496e9a2092fde412d1ec6ddb1dbd
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-03-06 17:12:32 +0900

    check 2

--- database/models.py
+++ database/models.py
@@ -1,9 +1,39 @@
-from sqlalchemy     import Boolean, Column, Integer, String, DateTime, ForeignKey, func
-from sqlalchemy.orm import *
-from database.settings       import Base
+import os
+
+from dotenv                     import load_dotenv
+from sqlalchemy                 import create_engine
+from sqlalchemy.orm             import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy                 import Boolean, Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy.orm             import *
+
+load_dotenv()
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST     = os.getenv("DB_HOST")
+DB_PORT     = os.getenv("DB_PORT")
+DB_DATABASE = os.getenv("DB_DATABASE")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+
+Engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
+
+
+# 모델링
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(120), nullable=False)
     name2 = Column(String(120), nullable=False)
+    name3 = Column(String(120), nullable=False)
